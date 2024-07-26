a6c0939ff52c8e31fdf7bf49eff5fb478ea33236
akash kumar <aakashkr7897@gmail.com>
2024-07-26 22:35:40 +0530

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -69,7 +69,7 @@ const Products = mongoose.model("Product",{
         type:Date,
         default:Date.now,
     },
-    avilable:{
+    available:{
         type:Boolean,
         default:true,
     },
@@ -104,7 +104,7 @@ app.post('/addproduct',async (req,res) =>{
 
 //Creating API for deleting Products
 
-app.post('/removeproduct' ,async(req,res)=>{
+app.post('/removeproduct' , async(req,res)=>{
     await Products.findOneAndDelete({id:req.body.id});
     console.log("Removed");
     res.json({
@@ -118,7 +118,84 @@ app.get('/allproducts', async(req,res)=>{
     let products = await Products.find({})
     console.log("All Products Fetched")
     res.send(products);
+});
+
+//Schema creating for user model
+
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+      type:String,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
 })
+
+//Creating end point for registering user
+app.post('/signup',async (req,res)=>{
+
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,errors:"existing user found with same email address"})
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i]=0;   
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:JSON.stringify(cart),
+    })
+
+    await user.save();
+
+    const data ={
+        user:{
+            id:user.id
+        }
+    }
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token})
+})
+
+//creating endpoint for user login
+app.post('/login',async(req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if(user) {
+        const passCompare = req.body.password === user.password
+        if(passCompare) {
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data, 'secret_ecom');
+            res.json({success:true,token});
+        }
+        else{
+            res.json({success:false,errors:"Wrong Password"});
+        }
+    }
+    else{
+        res.json({success:false,errors:"Wrong Email Id"})
+    }
+})
+
+
+//creatio
 
 app.listen(port, (error) => {
     if (!error) {
